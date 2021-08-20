b1876dc835ffbe40b53950f0ccbb4e7273d4b8c2
Nathan Pham <nathanpham.me@gmail.com>
2021-08-20 13:55:37 -0700

    tuples & arrays

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -20,4 +20,26 @@ fn main() {
     let r_string = "Random String";
     let r_string = r_string.len();
     println!("Final value of r_string: {}", r_string);
+
+    // must define type in order to parse strings
+    let guess: f32 = "42.1".parse().expect("This ain't it chief.");
+    println!("Guess: {}", guess);
+
+    // tuples
+    let tup: (i32, f64) = (1, 5.3);
+    println!("Tuple: {:?}", tup);
+
+    let (a, b) = tup;
+    println!("tup.0: {}, tup.1: {}", a, b);
+
+    // arrays (defined length)
+    // i32 is the type for 3 elements
+    let arr: [i32; 3] = [1, 2, 3];
+    println!("Array: {:?}", arr);
+
+    // array with repeated values (10)
+    let arr = [3; 10];
+    println!("Array: {:?}", arr);
+
+    println!("Array[0]: {}", arr[0]);
 }
