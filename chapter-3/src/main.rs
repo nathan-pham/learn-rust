102be23839861fc8f20092e646a363770d90752f
Nathan Pham <nathanpham.me@gmail.com>
2021-08-20 13:31:32 -0700

    new concepts

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -1,7 +1,19 @@
+// constant, always immutable variable
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
+    println!("MAX_POINTS: {}", MAX_POINTS);
+
+    // mutable variable
     let mut x = 5;
     println!("Initial value of x: {}", x);
 
     x = 2;
     println!("Reassigned value of x: {}", x);
+
+    // shadowing
+    let y = 5;
+    let y = y + 1;
+    let y = y + 10;
+    println!("Final value of y: {}", y);
 }
