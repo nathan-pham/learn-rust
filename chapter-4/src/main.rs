5f9373a083afc0ef027b5181d1c56a75740daa84
Nathan Pham <nathanpham.me@gmail.com>
2021-08-22 00:20:52 -0700

    ownership

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello World");
+
+    // invalidates s, moves pointer to s1
+    let (s1, length) = calculate_length(s);
+    println!("string: {}, length: {}", s1, length);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    // simply returning (s, s.len()) would not work because s would be invalidated
+
+    let length = s.len();
+    (s, length)
 }
