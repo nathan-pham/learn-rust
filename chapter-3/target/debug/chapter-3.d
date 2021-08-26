255360941391d3e8d4152ba015618453a3c7286c
Nathan Pham <nathanpham.me@gmail.com>
2021-08-26 11:08:54 -0700

    better gitignore

--- chapter-3/target/debug/chapter-3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\nathan-pham\Desktop\projects\learn-rust\chapter-3\target\debug\chapter-3.exe: C:\Users\nathan-pham\Desktop\projects\learn-rust\chapter-3\src\main.rs
--- chapter-3/target/debug/deps/chapter_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\nathan-pham\Desktop\projects\learn-rust\chapter-3\target\debug\deps\chapter_3.exe: src\main.rs
-
-C:\Users\nathan-pham\Desktop\projects\learn-rust\chapter-3\target\debug\deps\chapter_3.d: src\main.rs
-
-src\main.rs:
