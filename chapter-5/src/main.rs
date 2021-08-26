17a3ca866a460c2f993d69fd31e1ce62ae4cf787
Nathan Pham <nathanpham.me@gmail.com>
2021-08-26 11:26:37 -0700

    struct constructors

--- chapter-5/src/main.rs
+++ chapter-5/src/main.rs
@@ -5,18 +5,54 @@ struct User {
     active: bool
 }
 
-// declare tuple structs
+// declare tuple struct
+#[derive(Debug)]
 struct Point(i32, i32, i32);
+
+// declare a rectangle struct
+// enables println! with debugging information
+#[derive(Debug)]
+struct Rectangle { 
+    width: u32, 
+    height: u32 
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        self.area() > rectangle.area()
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+}
 
 fn main() {
     let user = create_user(String::from("Nathan Pham"), String::from("nathanpham.me@gmail.com"));
-    print_user(user);
 
     // "spread" syntax, only applies to attributes that can be copied
     let extended = User { name: String::from("Bruh"), email: String::from("bruh@gmail.com"), ..user };
+    print_user(user);
     print_user(extended);
 
-    let point = Point(0, 0, 0);
+    let point = Point(0, 0, 0); 
+    println!("point: {:?}", point);
+
+    let rectangle = Rectangle { width: 100, height: 100 };
+    let rectangle2 = Rectangle { width: 150, height: 150 };
+    println!("rectangle: {:?}", rectangle);
+    println!("rectangle area 1: {}", rectangle.area());
+    println!("rectangle area 2: {}", rectangle2.area());
+    println!("can rectangle 1 hold rectangle 2? {}", rectangle.can_hold(&rectangle2));
+
+    println!("rectangle 3: {:?}", Rectangle::square(3));
 }
 
 // create instance of a struct
